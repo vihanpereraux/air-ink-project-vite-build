c1609316b962d33d354a586f62a59f71acb08d5e
Vihan Perera <vihanpereraux@gmail.com>
2023-07-11 13:17:27 +0530

    Initial coomit

--- p5-js-build/sketch.js
+++ p5-js-build/sketch.js
@@ -1,41 +1,31 @@
 let canvasWidth = window.innerWidth;
 let canvasHeight = window.innerHeight;
-let mouseXPosition;
-let mouseYPosition;
-let lines = [];
-let xValues = [];
-let yValues = [];
-let points = [];
-let curveValues = (10, 30, 60, 150, 10, 600, 30, 90);
-
-// stats
-let linesCount = document.getElementById('lines-count');
-
-// properties 
-let strokeColor, customStrokeWeight;
+let randomValue;
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
-  background(20);
+  background(10);
 }
 
 function draw() {
-  // display stats
-  linesCount.innerText = lines.length;
+  randomValue = random(100, 250)
 
-  // drawing function
   if(mouseIsPressed){
-    let point = { x:mouseX, y:mouseY } 
-    points.push(point);
+    noStroke();
+    for (let index = 0; index < 20; index++) {
+      fill(random(0, 255), random(0, 255), random(0, 255));
+      circle(
+        mouseX + random(randomValue, -randomValue), 
+        mouseY + random(randomValue, -randomValue), 
+        3); 
+    }
 
-    strokeColor = 255;
-    customStrokeWeight = 0.1;
-    var myLine = new MyLine();
-    myLine.setDetails(strokeColor, customStrokeWeight);
-    lines.push(myLine);
-  }
-
-  for (let index = 0; index < lines.length; index++) {
-    lines[index].show();    
+    for (let index = 0; index < 20; index++) {
+      fill(255);
+      strokeWeight(4);
+      line(
+        mouseX, mouseY, 
+        600, 900)
+    }
   }
 }
