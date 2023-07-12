f6aadc15b08d2a293aa2437a806ca99c68e2acbe
Vihan Perera <vihanpereraux@gmail.com>
2023-07-12 14:52:19 +0530

    Tesh shapes

--- p5-js-build/sketch.js
+++ p5-js-build/sketch.js
@@ -1,6 +1,8 @@
 let canvasWidth = window.innerWidth;
 let canvasHeight = window.innerHeight;
 let randomValue;
+let strokeSize;
+let points = [];
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
@@ -8,24 +10,50 @@ function setup() {
 }
 
 function draw() {
-  randomValue = random(100, 250)
+  strokeSize = 3;
+  noFill();
+  stroke(255);
+
+  strokeCap();
+  strokeWeight(strokeSize);
+  beginShape();
+    curveVertex(200, 80);
+    curveVertex(200, 80);
+    curveVertex(400, 200);
+    curveVertex(700, 100);
+    curveVertex(900, 400);
+    curveVertex(900, 400);
+  endShape();
 
   if(mouseIsPressed){
-    noStroke();
-    for (let index = 0; index < 20; index++) {
-      fill(random(0, 255), random(0, 255), random(0, 255));
-      circle(
-        mouseX + random(randomValue, -randomValue), 
-        mouseY + random(randomValue, -randomValue), 
-        3); 
-    }
+    let point = { x: mouseX, y: mouseY }
+    points.push(point);
 
-    for (let index = 0; index < 20; index++) {
-      fill(255);
-      strokeWeight(4);
-      line(
-        mouseX, mouseY, 
-        600, 900)
+    strokeWeight(strokeSize + 3);
+    beginShape();
+    for (let index = 0; index < points.length; index++) {
+      if(index == 0){
+        curveVertex(points[index].x, points[index].y);
+        curveVertex(points[index].x, points[index].y);
+      }
+      else if(index == (points.length -1)){
+        curveVertex(points[index].x, points[index].y);
+        curveVertex(points[index].x, points[index].y);
+      }
+      else{
+        curveVertex(points[index].x, points[index].y);
+      }
+      // index = index + 1;
     }
+    endShape();
+  }
+  else{
+    points = [];
+  }
+}
+
+function keyPressed(){
+  if(keyCode === LEFT_ARROW){
+    console.log(points)
   }
 }
